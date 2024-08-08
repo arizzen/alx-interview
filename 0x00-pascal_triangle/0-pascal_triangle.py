68e4516bafe63128871c7145e80d88aeefed7def
arizzen <a1.arizzen@gmail.com>
2024-08-08 10:21:09 -0700

    triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        row = [1]  # First element of each row is 1
+        for j in range(1, i):
+            # Each element is the sum of the two elements above it
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # Last element of each row is 1
+        triangle.append(row)
+    
+    return triangle
